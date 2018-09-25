52d4976165d341a36603284307e91046b633a3e2
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 17:26:01 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -12,6 +12,16 @@ function getRepositories() {
 }
 
 function showRepos(){
-  let repoDiv = document.getElementById('repositories')
+  document.getElementById('repositories').innerHTML = repoList;
 
 }
+
+
+// function showRepositories() {
+//   var repos = JSON.parse(this.responseText);
+//   console.log(repos);
+//   const repoList = `<ul>${repos
+//     .map(r => '<li>' + r.name + '</li>')
+//     .join('')}</ul>`;
+//   document.getElementById('repositories').innerHTML = repoList;
+// }
