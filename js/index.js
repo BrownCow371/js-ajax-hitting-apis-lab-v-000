fb813cb5e66e03801b19eff1f2d51ffa501770a4
Jamie Brown <jmbrowncow@gmail.com>
2018-09-25 17:15:48 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // your code here
+// Create a form with a username field that calls a getRepositories function that loads the repositories div with a list of public repositories for that user.
+// The displayed repositories should include the name and a link to the URL (HTML URL, not API URL).
+
+function getRepositories() {
+  let user = document.getElementById('username'.value)
+}
